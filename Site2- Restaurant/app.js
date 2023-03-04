1290a2c25a9bd1035fecfd88074b43dc0e271b14
DzedajMuharem <mujanovich100@gmail.com>
2023-03-04 21:27:53 +0100

    Site2

--- Site2- Restaurant/app.js
+++ Site2- Restaurant/app.js
@@ -1,3 +1,26 @@
 const filter = (button) => {
   let category = button.getAttribute("data-category");
+  let portfolioItems = document.querySelectorAll(".menu-card2");
+  console.log(portfolioItems);
+  portfolioItems.forEach((item) => {
+    item.style.display = "none";
+  });
+
+  if (category === "sve") {
+    portfolioItems.forEach((item) => {
+      item.style.display = "flex";
+    });
+  }
+  portfolioItems.forEach((item) => {
+    if (item.getAttribute("data-category").includes(category)) {
+      item.style.display = "flex";
+    }
+  });
 };
+
+const nazad = document.querySelector(".nazad");
+
+nazad.addEventListener("click", function () {
+  let options = "";
+  let ref = window.open("index.html", "_self", options);
+});
